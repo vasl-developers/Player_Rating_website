443fc9f3e0eff6b9e133d6607c13f268b352e79f
Sam Tyson <styson@gmail.com>
2020-06-01 22:55:42 -0500

    changed code to show list of tournament results

--- web/PHP/showgameresultstable.php
+++ web/PHP/showgameresultstable.php
@@ -1,89 +1,70 @@
 <?php
-  include("PHP/connection.php");
-  $mysqli = mysqli_connect($host, $username, $password, $database);
-  $sql = "select * from match_results WHERE Tournament_ID = " . $tournamenttoshow . " ORDER BY Round_No";
-  $result = mysqli_query($mysqli, $sql);
-
-  $firstarray = [];
-  $secondarray = [];
-  while ($row = mysqli_fetch_assoc($result)) {
-    $firstarray[] = $row;
-    $secondarray[] = $row;
+  include("./connection.php");
+  $mysqli = new mysqli($host, $username, $password, $database);
+  if (mysqli_connect_errno())
+  {
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    exit();
   }
-  $previousroundno="";
-  foreach ($firstarray as $row) {
-    if ($row["Round_No"] != $previousroundno) {
-      $previousroundno = $row["Round_No"];
+  $mysqli->set_charset("utf8");
+
+  $sql = "select Player1_Namecode, Player1_AttDef, Player1_AlliesAxis, Player2_Namecode, Player2_AttDef, Player2_AlliesAxis, Round_No, Scenario_ID, player1.Fullname, player2.Fullname from match_results INNER JOIN players player1 ON player1.Player_Namecode=match_results.Player1_Namecode INNER JOIN players player2 ON player2.Player_Namecode=match_results.Player2_Namecode where Tournament_ID = '" . $_GET["tournamentid"] . "' order by Round_No";
+
+  if ($stmt = $mysqli->prepare($sql)) {
+    $stmt->execute();
+    $stmt->bind_result($p1Code, $p1AttDef, $p1AlliAxis, $p2Code, $p2AttDef, $p2AlliAxis, $roundNo, $scenario, $player1, $player2);
+
+    $previousRoundNo="";
 ?>
-<html>
-<h1>Round <?php echo $row["Round_No"]?></h1>
-<table class="table table-condensed table-striped">
-  <thead>
-  <tr>
-    <th>Player</th>
-    <th>Att/Def</th>
-    <th>Al/Ax</th>
-    <th>Result</th>
-    <th>Player</th>
-    <th>Att/Def</th>
-    <th>Al/Ax</th>
-    <th></th>
-    <th>Scenario</th>
-  </tr>
-  </thead>
-  <tbody>
-  <?php
-  foreach ($secondarray as $row1) {
-    if ($row1["Round_No"]==$previousroundno) {
-      $playercode1 = trim($row1["Player1_Namecode"]);
-      $player1     = getplayername(trim($row1["Player1_Namecode"]));
-      $p1attdef    = trim($row1["Player1_AttDef"]);
-      $p1alax      = trim($row1["Player1_AlliesAxis"]);
-
-      $playercode2 = trim($row1["Player2_Namecode"]);
-      $player2     = getplayername(trim($row1["Player2_Namecode"]));
-      $p2attdef    = trim($row1["Player2_AttDef"]);
-      $p2alax      = trim($row1["Player2_AlliesAxis"]);
-
-      $scenario    = trim($row1["Scenario_ID"])
-  ?>
+    <table class="table table-condensed table-striped">
+      <thead>
       <tr>
-        <td class="top">
-          <p><a class="content" href="tablePlayerGameResults.php?playercode=<?php echo $playercode1?>"><?php echo $player1?></a></p>
-        </td>
-        <td><?php echo $p1attdef?></td>
-        <td><?php echo $p1alax?></td>
-        <td>beats</td>
-        <td class="top">
-          <p><a class="content" href="tablePlayerGameResults.php?playercode=<?php echo $playercode2?>"><?php echo $player2?></a></p>
-        </td>
-        <td><?php echo $p2attdef?></td>
-        <td><?php echo $p2alax?></td>
-        <td>in</td>
-        <td><?php echo $scenario?></td>
-    </tr>
-  <?php
-    }
-  }
-  ?>
-  </tbody>
-</table>
-</html>
+        <th>Player</th>
+        <th>Att/Def</th>
+        <th>Al/Ax</th>
+        <th>Result</th>
+        <th>Player</th>
+        <th>Att/Def</th>
+        <th>Al/Ax</th>
+        <th></th>
+        <th>Scenario</th>
+      </tr>
+      </thead>
+      <tbody>
+        <?php
+        while ($row = $stmt->fetch()) {
+          if ($roundNo != $previousRoundNo) {
+            $previousRoundNo = $roundNo;
+        ?>
+        <tr>
+          <td colspan=9>Round <?php echo $roundNo ?></th>
+        </tr>
+          <?php
+          }
+          ?>
+        <tr>
+          <td class="top">
+            <p><a class="content" href="tablePlayerGameResults.php?playercode=<?php echo $p1Code ?>"><?php echo $player1 ?></a></p>
+          </td>
+          <td><?php echo $p1AttDef ?></td>
+          <td><?php echo $p1AlliAxis ?></td>
+          <td>beats</td>
+          <td class="top">
+            <p><a class="content" href="tablePlayerGameResults.php?playercode=<?php echo $p2Code ?>"><?php echo $player2 ?></a></p>
+          </td>
+          <td><?php echo $p2AttDef ?></td>
+          <td><?php echo $p2AlliAxis ?></td>
+          <td>in</td>
+          <td><?php echo $scenario ?></td>
+        </tr>
+        <?php
+          }
+        ?>
+      </tbody>
+    </table>
 <?php
+    $mysqli->close();
+  } else {
+    echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
   }
-}
-$mysqli->close();
-
-// Defining function
-function getplayername($playercode){
-  global $mysqli;
-  $playercode=trim($playercode);
-  $stmt5=$mysqli->prepare("SELECT * FROM players WHERE Player_Namecode=?");
-  $stmt5->bind_param("s", $playercode);
-  $stmt5->execute();
-  $result5=$stmt5->get_result(); // get the mysqli result
-  while ($row5 = $result5->fetch_assoc()) {
-    $pname = $row5["Fullname"];
-    return $pname;
-  }
-}
+?>
