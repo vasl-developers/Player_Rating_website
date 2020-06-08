3f1fefd782dac3c9a12a0b2996e03aa592bf2456
Sam Tyson <styson@gmail.com>
2020-06-08 10:02:41 -0500

    fix href tags

--- web/include/right-sidebar.php
+++ web/include/right-sidebar.php
@@ -3,14 +3,14 @@
     <a href="#" class="list-group-item active">
       <h4 class="list-group-item-heading">Tools and Support</h4>
     </a>
-    <a class="list-group-item content" data-href="web/Tools_Support/UploadTournamentResults.html">Upload Tournament Data</a>
-    <a class="list-group-item content" href="web/files/ASL Player Ratings Game Results Data Form.xlsx">Download Data Entry Form</a>
-    <a class="list-group-item content" data-href="web/PHP/Tools/DownloadPlayerList.php">Download Player List</a>
-    <a class="list-group-item content" data-href="web/Tools_Support/DownloadScenarioList.html">Download Scenario List</a>
-    <a class="list-group-item content" data-href="web/Tools_Support/GetHelp.html">Get Help</a>
-    <a class="list-group-item content" data-href="web/Tools_Support/GetTheData.html">Get The Data!</a>
-    <a class="list-group-item content" href="web/pages/toolSubmitGameCorrection.php">Submit A Correction</a>
-    <a class="list-group-item content" data-href="web/PHP/UpdateTables/updateplayers.php">Add or Update Players</a>
-    <a class="list-group-item content" data-href="web/Tools_Support/HideYourData.html">Hide Your Data</a>
+    <a class="list-group-item" href="web/Tools_Support/UploadTournamentResults.html">Upload Tournament Data</a>
+    <a class="list-group-item" href="web/files/ASL Player Ratings Game Results Data Form.xlsx">Download Data Entry Form</a>
+    <a class="list-group-item" href="web/PHP/Tools/DownloadPlayerList.php">Download Player List</a>
+    <a class="list-group-item" href="web/Tools_Support/DownloadScenarioList.html">Download Scenario List</a>
+    <a class="list-group-item" href="web/Tools_Support/GetHelp.html">Get Help</a>
+    <a class="list-group-item" href="web/Tools_Support/GetTheData.html">Get The Data!</a>
+    <a class="list-group-item" href="web/pages/toolSubmitGameCorrection.php">Submit A Correction</a>
+    <a class="list-group-item" href="web/PHP/UpdateTables/updateplayers.php">Add or Update Players</a>
+    <a class="list-group-item" href="web/Tools_Support/HideYourData.html">Hide Your Data</a>
   </div>
 </div>
