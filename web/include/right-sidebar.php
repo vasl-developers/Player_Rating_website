227421af30e0a146e99ffbd2900ef71de0aed90c
Doug Rimmer <dougrimmer@rogers.com>
2020-06-14 22:19:48 -0400

    #14 final additions to Tools menu

--- web/include/right-sidebar.php
+++ web/include/right-sidebar.php
@@ -8,14 +8,15 @@ set_include_path($_SERVER['DOCUMENT_ROOT']);
       <h4 class="list-group-item-heading">Tools and Support</h4>
     </a>
     <a class="list-group-item" href="<?php echo $ROOT; ?>web/pages/toolUploadTournamentResults.php">Upload Tournament Data</a>
-    <a class="list-group-item" href="<?php echo $ROOT; ?>web/Data/downloads/ASL Player Ratings Game Results Data Form.xlsx">Download Data Entry Form</a>
-    <a class="list-group-item" href="<?php echo $ROOT; ?>web/pages/toolDownloadPlayerList.php">Download Player List</a>
-    <a class="list-group-item" href="<?php echo $ROOT; ?>web/Tools_Support/DownloadScenarioList.html">Download Scenario List</a>
-    <a class="list-group-item" href="<?php echo $ROOT; ?>web/Tools_Support/GetHelp.html">Get Help</a>
-    <a class="list-group-item" href="<?php echo $ROOT; ?>web/pages/toolGetTheData.php">Get The Data!</a>
     <a class="list-group-item" href="<?php echo $ROOT; ?>web/pages/toolSubmitGameCorrection.php">Submit A Correction</a>
     <a class="list-group-item" href="<?php echo $ROOT; ?>web/pages/toolUpdatePlayers.php">Add or Update Players</a>
     <a class="list-group-item" href="<?php echo $ROOT; ?>web/pages/toolUpdateTournaments.php">Update A Tournament</a>
+    <a class="list-group-item" href="<?php echo $ROOT; ?>web/Data/downloads/ASL Player Ratings Game Results Data Form.xlsx">Download Data Entry Form</a>
+    <a class="list-group-item" href="<?php echo $ROOT; ?>web/pages/toolDownloadPlayerList.php">Download Player List</a>
+    <a class="list-group-item">Download Scenario List</a>
+    <a class="list-group-item" href="<?php echo $ROOT; ?>web/pages/toolGetTheData.php">Get The Data!</a>
     <a class="list-group-item" href="<?php echo $ROOT; ?>web/pages/toolHideYourData.php">Hide Your Data</a>
+    <a href="#" class="list-group-item active"><h4 class="list-group-item-heading">Help</h4></a>
+    <a class="list-group-item" href="<?php echo $ROOT; ?>web/pages/toolGetHelp.php">Get Help</a>
   </div>
 </div>
