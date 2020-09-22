7c185d4077d84402d17ad5c1c0d6a938ffb1aa0d
Sam Tyson <styson@gmail.com>
2020-09-22 12:26:10 -0500

    update fix page

--- web/pages/fixMatch.php
+++ web/pages/fixMatch.php
@@ -9,17 +9,18 @@ if (mysqli_connect_errno()) {
 $mysqli->set_charset("utf8");
 // get game results for tournament
 // and Scenario_ID like 'CH%'
-$sql = "select Scenario_ID, SUBSTRING_INDEX(Scenario_ID, ' ', 1), count(*) ct from match_results WHERE Length(Scenario_ID) > 10
+$sql = "select id, Scenario_ID, SUBSTRING_INDEX(Scenario_ID, ' ', 1), count(*) ct from match_results
+  WHERE Length(Scenario_ID) > 10 and Scenario_ID NOT LIKE 'SHELLSHOCK%'
   group by Scenario_ID
-  ORDER BY ct desc, Scenario_ID limit 50";
+  ORDER BY ct desc, Scenario_ID limit 5000";
 if (!($stmt = $mysqli->prepare($sql))) {
 	echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
 } else {
 	$stmt->execute();
-	$stmt->bind_result($scenarioId, $split, $count);
+	$stmt->bind_result($id, $scenarioId, $split, $count);
 	while ($row = $stmt->fetch()) {
 		?>
-      update match_results set scenario_id = '<?PHP echo $split; ?>' where scenario_id = '<?PHP echo $scenarioId; ?>'; // <?PHP echo $count; ?> <br/>
+      update match_results set scenario_id = '<?PHP echo $split; ?>' where scenario_id = '<?PHP echo $scenarioId; ?>';<br/>
 <?php
 }
 }
