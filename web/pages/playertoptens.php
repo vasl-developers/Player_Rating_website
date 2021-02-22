35706787e34f15fd805d024bd1b41a5dccafc674
Doug Rimmer <dougrimmer@rogers.com>
2021-02-21 22:09:51 -0500

    adding new Top Ten statistic

--- web/pages/playertoptens.php
+++ web/pages/playertoptens.php
@@ -9,6 +9,7 @@ include_once "web/include/navbar.htm";
 include_once "web/pages/connection.php";
 include_once "web/pages/functions.php";
 $mysqli = new mysqli($host, $username, $password, $database);
+$mysqli2 = new mysqli($host, $username, $password, $database);
 if (mysqli_connect_errno()) {
 	echo "Failed to connect to MySQL: " . mysqli_connect_error();
 	exit();
@@ -171,9 +172,7 @@ $stmt->close();
           </div>
           <div class="col-md-3">
               <?php
-
-$firstcount=0; $secondcount=0; $thirdcount=0;
-
+              $firstcount=0; $secondcount=0; $thirdcount=0;
               $sql3 = "SELECT Fullname, Player1_Namecode FROM player_ratings";
               if ($stmt3 = $mysqli->prepare($sql3)) {
                   $stmt3->execute();
@@ -186,56 +185,122 @@ $firstcount=0; $secondcount=0; $thirdcount=0;
                       $playerscore[$pnc] = round($tournamentfinishscore,1);
                   }
               }
-              //$stmt3->close();
-//$sql1 = "SELECT player_ratings.Fullname, player_ratings.Player1_Namecode FROM player_ratings";
-//if ($stmt1 = $mysqli->prepare($sql1)) {
-//	$stmt1->execute();
-//	$stmt1->bind_result($ffullname, $ppnc);
-//    while ($row1 = $stmt1->fetch()){
-//        $passplayercode = $ppnc;
-//        include "web/pages/tournamentfinishweighting.php";
-//        $playername[$ppnc] = trim($ffullname);
-//        $playerscore[$ppnc] = $tournamentfinishscore;
-//    }
-    arsort($playerscore);
-    $topten =0;
-	?>
+              arsort($playerscore);
+              $topten =0;
+              ?>
+              <div class="tableFixHead autoHeight">
+                  <table class="table table-sm table-striped table-hover">
+                      <thead>
+                      <tr>
+                          <th>Player</th>
+                          <th>Tournament Wins Score</th>
+                      </tr>
+                      </thead>
+                      <tbody>
+                      <?php
+                      foreach ($playerscore as $topscore) {
+                          $key = array_search (strval($topscore), $playerscore);
+                          if ($key == false) {
+                              $name = "Missing";
+                          } else {
+                              $name = trim($playername[$key]);
+                          }
+                          $topten +=1;
+                          ?>
+                          <tr>
+                              <td><a class="content" href="../pages/tablePlayerGameResults.php?playercode=<?php echo $pnc ?>"><?php echo prettyname($name) ?></a></td>
+                              <td><?php echo round($topscore,0) ?></td>
+                          </tr>
+                          <?php
+                          if($topten ==10){break;}
+                      }
+	                      ?>
+                      </tbody>
+                  </table>
+              </div>
+              <?php
+$stmt3->close();
+              ?>
+          </div>
+          <div class="col-md-3">
+              <?php
+
+              //$sql = "select m.Player1_Namecode AS p FROM match_results m , m.Player2_Namecode from match_results m WHERE m.Player1_Namecode=? OR m.Player2_Namecode=?";
+
+              $sql3 = "SELECT Fullname, Player1_Namecode FROM player_ratings";
+              if ($stmt3 = $mysqli->prepare($sql3)) {
+                  $stmt3->execute();
+                  $stmt3->bind_result($fullname, $pnc);
+                  while ($row = $stmt3->fetch()) {
+                      $NumberOpponents = 0;
+                      $passplayercode = $pnc;
+                      //$playername[$pnc] = trim($fullname);
+                      $playerOpps[$pnc][0] = $pnc;
+                      $playerOpps[$pnc][1] = trim($fullname);
+                  }
+              }
+              $stmt3->close();
+              foreach ($playerOpps as $topopps){
+                  //$key = array_search (strval($topopps), $playerOpps);
+                  //if ($key == false) {
+
+                  //} else {
+                      $passplayercode=$topopps[0];
+                      $sql2 = "SELECT p, COUNT(*) AS c FROM (SELECT m.Player2_Namecode AS p FROM match_results m WHERE m.Player1_Namecode=? UNION ALL
+                      SELECT m.Player1_Namecode AS p FROM match_results m WHERE m.Player2_Namecode=?) AS tp GROUP BY p";
+                      if ($stmt3 = $mysqli->prepare($sql2)) {
+                          $stmt3->bind_param("ss", $passplayercode, $passplayercode);
+                          $stmt3->execute();
+                          $stmt3->store_result();
+                          $playerOpps[$passplayercode][2] = $stmt3->num_rows;
+                      } else {
+                          $reg=0;
+                      }
+                  //}
+              }
+              //arsort($playerOpps);
+              uasort($playerOpps, function($a, $b) {
+                  return $a[2] < $b[2];
+              });
+              $topten =0;
+              ?>
                   <div class="tableFixHead autoHeight">
                       <table class="table table-sm table-striped table-hover">
                           <thead>
                           <tr>
                               <th>Player</th>
-                              <th>Tournament Wins Score</th>
+                              <th>Different Opponents</th>
                           </tr>
                           </thead>
                           <tbody>
+
                           <?php
-                          foreach ($playerscore as $topscore) {
-                                $key = array_search (strval($topscore), $playerscore);
-                                if ($key == false) {
-                                    $name = "Missing";
-                                } else {
-                                    $name = trim($playername[$key]);
-                                }
-		                        $topten +=1;
-		                    ?>
-                                <tr>
-                                  <td><a class="content" href="../pages/tablePlayerGameResults.php?playercode=<?php echo $pnc ?>"><?php echo prettyname($name) ?></a></td>
-                                  <td><?php echo round($topscore,0) ?></td>
-                                </tr>
-                                <?php
-                                if($topten ==10){break;}
+                          foreach ($playerOpps as $topopps) {
+                              $name = trim($topopps[1]);
+                              $key = $topopps[0]; //array_search (strval($topopps), $playerOpps);
+                              //if ($key == false) {
+                              //    $name = "Missing";
+                              //} else {
+                              //    $name = trim($playername[$key]);
+                              //}
+                              $topten +=1;
+                          ?>
+                              <tr>
+                                  <td><a class="content" href="../pages/tablePlayerGameResults.php?playercode=<?php echo $key ?>"><?php echo prettyname($name) ?></a></td>
+                                  <td><?php echo $topopps[2] ?></td>
+                              </tr>
+                          <?php
+                              if($topten ==10){break;}
                           }
-	?>
+                          ?>
                           </tbody>
                       </table>
                   </div>
-                  <?php
+              <?php
+              $stmt3->close();
 
-$stmt3->close();
-?>
+              ?>
           </div>
-
 
       </div>
   </div>
