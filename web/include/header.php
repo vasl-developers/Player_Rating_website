379eded7f9c8646edb9a728cee89b06325cee1e4
Sam Tyson <styson@gmail.com>
2021-04-28 13:39:09 -0500

    add header icons

--- web/include/header.php
+++ web/include/header.php
@@ -6,6 +6,7 @@
   <link rel="shortcut icon" href="/web/images/favicon.ico" type="image/x-icon" />
   <link rel="icon" href="/web/images/favicon.ico" type="image/x-icon" />
   <title>ASL Player Ratings</title>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
   <link type="text/css" href="/web/css/styles.css" rel="stylesheet" />
   <!-- Global site tag (gtag.js) - Google Analytics -->
